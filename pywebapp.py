cccfa268aec9f75ae2dd87a0b02a212088689852
ryanhhtan <ryanhhtan@gmail.com>
2017-11-06 11:27:40 -0800

    changed background color to red

--- pywebapp.py
+++ pywebapp.py
@@ -13,7 +13,7 @@ class Default:
         hostname = socket.gethostname()
         return """<html>
         <title>PyWebApp</title>
-        <body bgcolor="green">
+        <body bgcolor="red">
         <h1>Hello COMP4913!</h1>
         <p>Available API methods:</p>
         <ul>
